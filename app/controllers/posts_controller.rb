62ce6d5aeaf76e48f53206e34cfbed224dbb9bc3
ahmedootta <ahmedfadl3019@gmail.com>
2024-09-22 01:21:05 +0300

    Handle update post including his tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # disable CSRF middleware
   protect_from_forgery with: :null_session
 
-  # insert new post
+  # INSERT NEW POST
   def create
     user = get_logged_user
     tags = post_params[:tags]
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   )
   end
   
-  # get post by_id
+  # GET POST BY ID
   def show
     user = get_logged_user
     id  = params.permit(:id)['id'].to_i
@@ -45,16 +45,31 @@ class PostsController < ApplicationController
     end  
   end
 
+  # UPDATE POST INCLUDED TAGS BY THE AUTHOR
   def update
     user_id = get_logged_user.id
     post_id = params.permit(:id)['id'].to_i
-    
     begin
       target_post = Post.find(post_id)
       if target_post.author_id == user_id
         begin
-          # code to update post...
-          render json: { message: "Post has been updated!" }
+          keys_to_update = post_params.except(:tags)
+          tags = post_params[:tags]
+          keys_to_update.each do|key, value|
+            target_post.update("#{key}": value)
+          end  
+          if tags
+            # delete old tags for this post
+            PostTag.destroy_by(post_id: post_id)
+            for index in 0...tags.length
+              target_tag = Tag.find_by(name: tags[index]) 
+              PostTag.create(post_id: post_id, tag_id: target_tag.id)
+            end 
+          else  
+            print "Tags never sent!"
+            puts "\n"
+          end   
+          render json: { message: "Post has been updated" }
         rescue
           render json: { error: "Failed to update!"}, status: unprocessable_entity
         end 
@@ -66,6 +81,7 @@ class PostsController < ApplicationController
     end      
   end
   
+  # DELETE POST BY ITS AUTHOR
   def destroy
     user_id = get_logged_user.id
     post_id = params.permit(:id)['id'].to_i
@@ -86,8 +102,6 @@ class PostsController < ApplicationController
       render json: { error: "Post doesn't exist!" }
     end      
   end
-
-
 
   private
   def post_params
